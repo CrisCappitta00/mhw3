59bbf02d3a93484b414f0c81c447c2dd296ae556
Cristina <cppcst00e48f943w@studium.unict.it>
2023-04-30 17:59:42 +0200

    Js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 /*Creazione Api senza autenticazione */
 /*Creo una variabile che memorizza il primo elemento restituito da queryselector */
 const form = document.querySelector('form');
-fetch('/api/v3/NextPublicHolidays/{countryCode}').then(onResponse).then(onJson);
+fetch('/api/v3/NextPublicHolidays/JP').then(onResponse).then(onJson);
 
+function onResponse(response){
+    if(response.ok){
+     return response.json();
+    }
 
+}
