a605ebbed3321ef4c1c0cb09c5b308bcb5aa2a24
Cristina <cppcst00e48f943w@studium.unict.it>
2023-04-30 17:19:49 +0200

    Js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 /*Creazione Api senza autenticazione */
+/*Creo una variabile che memorizza il primo elemento restituito da queryselector */
 const form = document.querySelector('form');
 form.addEventListener('submit', ricerca);
+
+function ricerca(event){
+    event.preventDefault();
+    const nome_festivita = document.querySelector('#Festività');
+}
