3445e93a698b1b22619b58086254ad703d247c92
Cristina <cppcst00e48f943w@studium.unict.it>
2023-04-30 17:53:05 +0200

    Modificato

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 /*Creazione Api senza autenticazione */
 /*Creo una variabile che memorizza il primo elemento restituito da queryselector */
 const form = document.querySelector('form');
-/*Aggiungo un eventlistener che si attiva quando clicco su Cerca*/
-form.addEventListener('submit', ricerca);
+fetch('/api/v3/NextPublicHolidays/{countryCode}').then(onResponse).then(onJson);
 
-function ricerca(event){
-    event.preventDefault();
-    const nome_festivita = document.querySelector('#Festività');
-    const valore = encodeURIComponent(nome_festivita.value);
-    console.log(valore);
-    /*Effettuare la richiesta con fetch */
-    fetch('https://date.nager.at/api/v2/publicholidays/2023/JP').then(onResponse).then(onJson);
-}
+
