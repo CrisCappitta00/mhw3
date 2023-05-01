c84f6a49a37307c9ab5ffd26f24990d2bd10c365
Cristina <cppcst00e48f943w@studium.unict.it>
2023-05-01 17:26:12 +0200

    Modificato

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 /*Creazione Api senza autenticazione */
 /*Creo una variabile che memorizza il primo elemento restituito da queryselector */
 const section = document.querySelector('section');
-fetch('https://api.sunrisesunset.io/json?lat=36.474328641705696&lng=138.72064733614394&timezone=GMT+9&date=today').then(onResponse).then(onJson);
-
-function onResponse(response){
-    if(response.ok){
-     return response.json();
-    }
+const url = "https://api.sunrisesunset.io/json?lat=36.474328641705696&lng=138.72064733614394&timezone=GMT+9&date=today";
+fetch(url).then(onSuccess, onError).then(onJson);
+function onSuccess(Response){
+  if(Response.ok){
+    console.log(Response);
+    return Response.json();
+  }
 }
-
-function onJson(json){
-    console.log('Json received'); 
+function onJson(Json){
+     console.log(Json);
+}
+function onError(Error){
+    console.error(Error);
 }
